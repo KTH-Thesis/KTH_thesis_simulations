b1cda313f81f1f3974b5da3936053d22daef2dd0
alexandros filotheou <alefil@kth.se>
2017-04-23 19:05:33 +0200

    D:ON (1,2) base

--- 3D/disturbances:ON/agents:1/obstacles:2/system_ct_1_real.m
+++ 3D/disturbances:ON/agents:1/obstacles:2/system_ct_1_real.m
@@ -3,14 +3,14 @@ function dx = system_ct_1(t, e, u, T)
   % Desired placements (destinations)
   global des_1;
 
-  % The supremum of the disturbance
-  global disturbance_ceiling;
+  % The disturbance
+  global disturbance;
 
   % State vector of agent 1:
   % [x,y]
   state = e' + des_1;
 
-  d = disturbance_ceiling * sin(2*t);
+  d = disturbance * sin(2*t);
 
   f1 = u(1) * cos(state(3)) + d;
   f2 = u(1) * sin(state(3)) + d;
